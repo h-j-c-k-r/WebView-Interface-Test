7a1d42d37e49080a420c618dd12c41825bf688b4
Hijacker <hijackervlad@yandex.ru>
2022-09-01 19:01:46 +0400

    initial commit

---
+++ app/src/main/java/com/example/webview_interface_test/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.webview_interface_test
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.webkit.JavascriptInterface
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textInputLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
+        val textInputEditText = findViewById<TextInputEditText>(R.id.textInputEditText)
+        val button = findViewById<MaterialButton>(R.id.button)
+        val webView = findViewById<WebView>(R.id.webView)
+
+        button.setOnClickListener {
+            setupWebView(textInputEditText.text.toString())
+            textInputLayout.isVisible = false
+            button.isVisible = false
+            webView.isVisible = true
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
+    private fun setupWebView(streamUrl: String) {
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.apply {
+            setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+            with(settings) {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                javaScriptCanOpenWindowsAutomatically = true
+                loadWithOverviewMode = true
+            }
+
+            addJavascriptInterface(
+                object {
+                    @JavascriptInterface
+                    fun copyToClipboard(text: String?) {
+                        showToast("copy", text)
+                    }
+
+                    @JavascriptInterface
+                    fun share(url: String?) {
+                        showToast("share", url)
+                    }
+
+                    @JavascriptInterface
+                    fun handleUserAction(param: String, action: String) {
+                        showToast("handle", param, action)
+                    }
+                },
+                "NativeAndroid"
+            )
+
+            loadUrl(streamUrl)
+        }
+    }
+
+    private fun showToast(vararg params: String?) {
+        Toast.makeText(this, params.joinToString(", "), Toast.LENGTH_SHORT).show()
+    }
+}
